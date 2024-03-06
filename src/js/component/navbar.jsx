1942e7a37b7879cc38fdf1fa06a2d4d3a659c69d
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2024-03-06 23:19:08 +0000

    formatting

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,30 +2,30 @@ import React from "react";
 const Nav = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
             <div className="container-fluid">
-            <a className="navbar-brand text-light" href="#">Brand</a>
-            <button className="navbar-toggler border-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-            <li className="nav-item">
-          <a className="nav-link active" style={{color: "#fdc57b"}} aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" style={{color: "#fdc57b"}} href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" style={{color: "#fdc57b"}} href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" style={{color: "#fdc57b"}}>About</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+              <a className="navbar-brand text-light" href="#">Brand</a>
+              <button className="navbar-toggler border-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a className="nav-link active" style={{color: "#fdc57b"}} aria-current="page" href="#">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" style={{color: "#fdc57b"}} href="#">Features</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" style={{color: "#fdc57b"}} href="#">Pricing</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" style={{color: "#fdc57b"}}>About</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
         </>
     )
 };
